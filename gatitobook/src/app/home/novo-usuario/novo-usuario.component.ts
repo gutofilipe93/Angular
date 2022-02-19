3d5326574b6b5d78cf7ccf3e6119c01729e459cb
Gustavo Filipe <gutofilipe93@gmail.com>
2022-02-19 17:02:39 -0300

    adicionado o cadastro de usuário e atualizado a versão para angular 13

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -15,7 +16,7 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService, private usuarioExistenteServive: UsuarioExistService) {}
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService, private usuarioExistenteServive: UsuarioExistService, private router: Router) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group(
@@ -36,8 +37,13 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar(){
-    const novoUsuario= this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if(this.novoUsuarioForm.valid){
+      const novoUsuario= this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() =>{
+        this.router.navigate(['']);
+      },(error) => {
+        console.log(error);
+      });
+    }
   }
-
 }

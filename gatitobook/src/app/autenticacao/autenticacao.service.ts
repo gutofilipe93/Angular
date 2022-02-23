2db666cd758f13bbca8fc0961f9fa6fc08cc37b2
Gustavo Barbosa <gustavo@idever.com.br>
2022-02-23 20:17:50 -0300

    adicionado o token na sessão e decodificando ele

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {tap} from "rxjs/operators";
+import { UsuarioService } from './usuario/usuario.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +11,19 @@ import { Observable } from 'rxjs';
 export class AutenticacaoService {
 
     
-  constructor(private httpClient: HttpClient){}
+  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService){}
 
-  autenticar(usuario: string, senha: string): Observable<any>{
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>>{
     return this.httpClient.post("http://localhost:3000/user/login",{
       userName: usuario,
       password: senha
-    });
+    },{
+      observe: "response"
+    }).pipe(
+      tap((resp) => {
+        const authToken = resp.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvarToken(authToken);
+      })
+    )
   }
 }

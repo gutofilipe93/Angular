7bfeeb5992832c78dd925f360a21f5fb0370ded0
Gustavo Barbosa <gustavo@idever.com.br>
2022-03-15 20:09:09 -0300

    implementado detalhe de animais e o interceptor

--- controle_fluxo_de_navegação/src/app/animais/animais.service.ts
+++ controle_fluxo_de_navegação/src/app/animais/animais.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 
@@ -20,5 +20,11 @@ export class AnimaisService {
     return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`,{
       headers,
     });
+  }
+
+  buscarPorID(id: number): Observable<Animal>{
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append("x-access-token",token);
+    return this.httpClient.get<Animal>(`${API}/photos/${id}`, {headers});
   }
 }

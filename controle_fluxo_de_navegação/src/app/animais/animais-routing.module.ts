0c050ce097bb454e093b923947fd6ee28bb80017
Gustavo Barbosa <gustavo@idever.com.br>
2022-03-29 20:12:42 -0300

    Adiciondo o menu lateral e iniciado a tela para cadastrar novos animais

--- controle_fluxo_de_navegação/src/app/animais/animais-routing.module.ts
+++ controle_fluxo_de_navegação/src/app/animais/animais-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     resolve:{
       animais: ListaAnimaisResolver
     }
+  },
+  {
+    path: 'novo',
+    component: NovoAnimalComponent
   },
   {
     path: ':animalId',
